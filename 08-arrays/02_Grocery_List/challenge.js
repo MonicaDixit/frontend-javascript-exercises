b408d3318547bf96436491533bfec9aa8dd8e59e
Monica <mdixit81@gmail.com>
2014-12-17 20:35:35 -0800

    commiting my changes

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,38 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function addItem(item , arr){
 
-module.exports.reverseSortedList = undefined;
+ if (arr.length < 0)
+   return;
+
+  var matchFound = false;
+
+  for (var i = 0 ; i < arr.length; i++){
+   if (arr[i] === item){
+   	  matchFound = true;
+   	  break;
+   }
+  }
+
+  if (!matchFound)
+  	 arr.push(item);
+
+  	return arr;
+
+};
+
+module.exports.reverseSortedList = function reverseSortedList(arr){
+
+  var highest ;
+
+  for (var i = 0 ; i < arr.length; i++){
+  	if (arr[i] < arr[i+1]){
+
+  		 highest = arr[i+1];
+  		 arr[i+1] = arr[i];
+  		 arr[i] = highest;
+
+  	}
+  }
+
+  return arr;
+
+};
