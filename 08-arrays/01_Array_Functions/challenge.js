f54d323689fa9370b6b5bf9a179225568dc24c5d
Monica <mdixit81@gmail.com>
2014-12-17 20:34:39 -0800

    commiting my changes

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,34 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(arr){
+ if (arr.length < 2)
+ 	return ;
 
-module.exports.plusesEverywhere = undefined;
+ var newArray = [1];
 
-module.exports.arrayQuantityPlusOne = undefined;
+  while (arr.length > 0){
+
+  	newArray.push(arr.pop());
+
+  }
+
+  return newArray;
+
+};
+
+module.exports.plusesEverywhere = function plusesEverywhere(arr){
+	if (arr.length < 2)
+		return ;
+
+    var resultString = "";
+	for (var i = 0 ; i < arr.length ; i++){
+    resultString += arr[i] +  "+" ;
+	}
+    
+	resultString = resultString.substring(0, resultString.length-1);
+	return resultString;
+};
+
+module.exports.arrayQuantityPlusOne = function arrayQuantityPlusOne(arr){
+
+return arr.length + 1;
+
+};
