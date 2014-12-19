5f044507baa30e282de2f0d102295742fe21c174
Monica <mdixit81@gmail.com>
2014-12-18 22:52:41 -0800

    adding my changes

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,43 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function createCourse(courseTitle,courseDuration,courseStudents){
+	var myObject = {
+		title : courseTitle,
+		duration : courseDuration,
+		students : courseStudents,
+	};
 
-module.exports.addProperty = undefined;
+	return myObject
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object , newProp, newValue){
+	var prop = newProp ;
+	if  (object.hasOwnProperty(newProp))
+		return object;
+	else{
+		object[newProp] = newValue ;
+		return object;
+	}
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter){
+	return "Hello "+letter.recipient+",\n\n"+letter.msg+"\n\nSincerely,\n"+letter.sender;
+
+};
+
+module.exports.canIGet = function(item, money){
+
+	var priceChart = [
+     {"name" : "MacBook Air" , "price" : 999},
+     {"name" : "MacBook Pro" , "price" : 1299},
+     {"name" : "Mac Pro" , "price" : 2499},
+     {"name" : "Apple Sticker" , "price" : 1}
+	] ;
+
+	for (i = 0 ; i < priceChart.length;i++){
+		if (item === priceChart[i].name && money >= priceChart[i].price){
+			return true;
+		}
+	}
+
+	return false;
+
+};
